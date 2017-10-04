e1e71992c7a7e12c558c67a8f69bf8b5351802ae
user <adhamkl2000@gmail.com>
2017-10-04 10:53:45 +0300

    קק

--- app/src/main/java/com/klaib/adham/adhamshoppinglist/login.java
+++ app/src/main/java/com/klaib/adham/adhamshoppinglist/login.java
@@ -2,12 +2,20 @@ package com.klaib.adham.adhamshoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class login extends AppCompatActivity
+{
+    private EditText ETEMAIL;
+    private EditText ETPASSWPRD;
+    private Button BTNLOGIN;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.log_in);
+
     }
 }
